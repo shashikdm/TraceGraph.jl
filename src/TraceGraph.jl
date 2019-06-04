ef5ccdf6f8ed916b0ac39dd5b89cff82402d187c
shashikdm <shashikant.123kadam@gmail.com>
2019-06-04 18:40:43 +0530

    improve efficiency

--- src/TraceGraph.jl
+++ src/TraceGraph.jl
@@ -3,6 +3,36 @@ module TraceGraph
 using Cassette
 using LightGraphs
 
+"""
+    struct TNode
+Object corresponding to every vertex in graph
+fields:
+- name::String      : Unique name for each node
+- op::String        : Name of the function/operation
+- dtype::DataType   : datatype of content
+- size::Tuple       : size of the content
+"""
+
+struct TNode
+    name::String
+    op::String
+    dtype::DataType
+    size::Tuple
+end
+
+"""
+    struct TGraph
+Object that represents the traced graph of the function call.
+fields:
+- graph::SimpleDiGraph  : Object of type SimpleDiGraph
+- names::Vector{String} : List of names corresponding to the nodes
+"""
+struct TGraph
+    graph::SimpleDiGraph
+    nodelist::Vector{TNode}
+    labels::Vector{String}
+end
+
 include("GenerateGraph.jl")
 
 export TGraph, TNode, generategraph, ignorelist
