31754f5dd4fda7a1d42f382d946c7e34c1d22e0c
shashikdm <shashikant.123kadam@gmail.com>
2019-06-04 18:54:05 +0530

    rm debug info

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,6 @@ using LightGraphs
     tg = generategraph(+, 1, 2)
     @test nv(tg.graph) == 3
 
-
     function foo(x)
         x = x+1
     end
@@ -19,8 +18,6 @@ using LightGraphs
     end
     tg = generategraph(bar, 10)
     @test nv(tg.graph) == 7
-
-
 
     tg = generategraph(softmax, rand(2))
 end
