6dbd4613ac134e514cf34da37cf12f0bce525b8d
shashikdm <shashikant.123kadam@gmail.com>
2019-06-15 01:10:59 +0530

    Overhaul

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,15 @@
 using Test
 using TraceGraph
-using TraceGraph:generategraph
+using TraceGraph:tracegraph
 using GraphPlot
-using Flux
 using LightGraphs
 
 @testset "Basic Tests" begin
-    @time tg = generategraph(+, 1, 2)
-    @test nv(tg.graph) == 3
-
-    function foo(x)
-        x = x+1
+    function foo(a)
+            b = a+10
+            c = b+10
+            return (a,b,c)
     end
-
-    function bar(y)
-        y = foo(y-1)
-    end
-    @time tg = generategraph(bar, 10)
-    @test nv(tg.graph) == 7
-
-    @time tg = generategraph(softmax, rand(2))
-    @time tg = generategraph(Conv((3,3), 1=>1), rand(3,3,1,1))
+    gd = tracegraph(foo, 100)
+    gplothtml(gd.g, nodelabel = gd.nodelabel)
 end
