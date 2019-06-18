37a3f6d7c7be468a343c17f8955c548bbcd7bc9b
shashikdm <shashikant.123kadam@gmail.com>
2019-06-18 11:00:36 +0530

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,18 +4,18 @@ using TraceGraph:tracegraph
 using GraphPlot
 using LightGraphs
 
-# @testset "Basic Tests" begin
-#     function foo(a)
-#             b = a+10
-#             c = b+10
-#             return (a,b,c)
-#     end
-#     gd = tracegraph(foo, 100)
-#     gplothtml(gd.g, nodelabel = gd.nodelabel)
-#     add_norecurse(reshape)
-#     show_norecurse()
-#     rm_norecurse(reshape)
-# end
+@testset "Basic Tests" begin
+    function foo(a)
+            b = a+10
+            c = b+10
+            return (a,b,c)
+    end
+    gd = tracegraph(foo, 100)
+    gplothtml(gd.g, nodelabel = gd.nodelabel)
+    add_norecurse(reshape)
+    show_norecurse()
+    rm_norecurse(reshape)
+end
 @testset "Conditional Statement" begin
     function foo(a::Bool)
         if a == true
